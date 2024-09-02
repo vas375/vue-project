8754fe8ac5501c453ed0e1ecc8f7f426d0f24a14
vase375 <vasesen@ttostech.com>
2024-09-02 18:07:50 +0900

    init

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'root',
+      component: Home
     },
     {
       path: '/about',
---
+++ src/views/home.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>home</div>
+</template>
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+
+onMounted(() => {})
+</script>
+<style lang="scss" scoped></style>
